affb61cb29b582781f8cbd003517bca9fecac74f
sadmanryanriad <sadmanryanriad@gmail.com>
2024-06-19 18:32:13 +0600

    success story, event-list, image gallery api added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -27,7 +27,6 @@ async function run() {
     const upcomingEventImages = client
       .db("sge")
       .collection("upcoming-event-img");
-    const marqueeList = client.db("sge").collection("marquee-list");
 
     //get upcoming event images
     app.get("/upcoming-events", async (req, res) => {
@@ -41,6 +40,32 @@ async function run() {
           .send({ error: "An error occurred while fetching upcoming events" });
       }
     });
+    //delete event by id
+    app.delete("/upcoming-events/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await upcomingEventImages.deleteOne(query);
+        res.send(result);
+      } catch (error) {
+        req.send({ error: "An error occurred while fetching upcoming events" });
+      }
+    });
+    //add upcoming event image
+    app.post("/upcoming-events", async (req, res) => {
+      try {
+        const formData = req.body;
+        const result = await upcomingEventImages.insertOne(formData);
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching upcoming events:", error);
+        res
+          .status(500)
+          .send({ error: "An error occurred while fetching upcoming events" });
+      }
+    });
+
+    const marqueeList = client.db("sge").collection("marquee-list");
     //get marquee list
     app.get("/marquee-list", async (req, res) => {
       try {
@@ -53,6 +78,31 @@ async function run() {
           .send({ error: "An error occurred while fetching upcoming events" });
       }
     });
+    //delete marquee-list id
+    app.delete("/marquee-list/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await marqueeList.deleteOne(query);
+        res.send(result);
+      } catch (error) {
+        req.send({ error: "An error occurred while fetching upcoming events" });
+      }
+    });
+    //add marquee-list image
+    app.post("/marquee-list", async (req, res) => {
+      try {
+        const formData = req.body;
+        const result = await marqueeList.insertOne(formData);
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching upcoming events:", error);
+        res
+          .status(500)
+          .send({ error: "An error occurred while fetching upcoming events" });
+      }
+    });
+
     const SuccessStoryList = client.db("sge").collection("success-story-list");
     //get success-story-list
     app.get("/success-story-list", async (req, res) => {
@@ -66,6 +116,31 @@ async function run() {
           .send({ error: "An error occurred while fetching upcoming events" });
       }
     });
+    //add success-story-list
+    app.post("/success-story-list", async (req, res) => {
+      try {
+        const formData = req.body;
+        const result = await SuccessStoryList.insertOne(formData);
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching upcoming events:", error);
+        res
+          .status(500)
+          .send({ error: "An error occurred while fetching upcoming events" });
+      }
+    });
+    //delete success-story-list by id
+    app.delete("/success-story-list/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await SuccessStoryList.deleteOne(query);
+        res.send(result);
+      } catch (error) {
+        req.send({ error: "An error occurred while fetching upcoming events" });
+      }
+    });
+
     const eventList = client.db("sge").collection("event-list");
     //get event-list
     app.get("/event-list", async (req, res) => {
@@ -79,6 +154,32 @@ async function run() {
           .send({ error: "An error occurred while fetching upcoming events" });
       }
     });
+    //add success-story-list
+    app.post("/event-list", async (req, res) => {
+      try {
+        const formData = req.body;
+        const result = await eventList.insertOne(formData);
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching upcoming events:", error);
+        res
+          .status(500)
+          .send({ error: "An error occurred while fetching upcoming events" });
+      }
+    });
+    //delete success-story-list by id
+    app.delete("/event-list/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await eventList.deleteOne(query);
+        res.send(result);
+      } catch (error) {
+        req.send({ error: "An error occurred while fetching upcoming events" });
+      }
+    });
+
+
     const consultationForms = client.db("sge").collection("consultation-forms");
     //insert consultation forms data into database
     app.post("/consultation-forms", async (req, res) => {
